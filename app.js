3742593d6f7b62577e6812433cc48867e7112425
sibbe23 <sibbe23@gmail.com>
2023-02-28 11:02:12 +0530

    Body-parser has been used

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require('express')
 
+const bodyParser = require('body-parser')
+
 const app = express()
 
-app.use((req,res,next)=>{
-    console.log('This is a middleware')
-    next()
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use('/add-product',(req,res,next)=>{
+    // res.setHeader('content-type', 'text/html')
+    // res.set({'content-type': 'text/html'})
+    res.send('<form action="/product" method="POST"><input type="text" name="title"> <input type="number" name="size"><button type="submit">Add</button></form>')
 })
 
-app.use((req,res,next)=>{
-    console.log('This is 2 middleware')
+app.post('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use('/',(req,res,next)=>{
     // res.setHeader('content-type', 'text/html')
     // res.set({'content-type': 'text/html'})
     res.send('<h3> Hello There </h3>')
 })
+
+
 
 app.listen(2000)
